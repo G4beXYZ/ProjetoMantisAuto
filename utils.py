9c6257b09daf41c4afa23de9168b58ee27951139
G4be <62225558+G4beXYZ@users.noreply.github.com>
2021-06-17 16:58:45 -0300

    Update utils.py
    Trocada o valor das variáveis de USUARIO, SENHA, USUARIO_COMPLETO e CARGO .
    Leia o README.md para mais informações

--- utils.py
+++ utils.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.keys import Keys
 #[Aqui é onde fica a classe onde é armazenado algumas variáveis que serão utilizadas ao longo dos steps
 #  criados.]
 class UtilVars(object):
-    USUARIO = 'g4be'
-    SENHA = 'hCGxUDH49GtESN3'
-    USUARIO_COMPLETO = 'g4be ( Gabriel Canuto Miranda )'
-    CARGO = 'desenvolvedor'
+    USUARIO = 'INSIRA_USUARIO_AQUI'
+    SENHA = 'INSIRA_SENHA_AQUI'
+    USUARIO_COMPLETO = 'NOME_PAGINA_PRINCIPAL'
+    CARGO = 'CARGO_AQUI'
 
 #Aqui é a classe onde contém o acesso aos sites requisitados, no caso deste projeto, só foi utilizado 1 único link
 class Utils(Browser):
